94dd7e6e84ddcfff69c8b458c2b8aaa6653993be
4geru <westhouse51@gmail.com>
2024-07-07 14:49:17 +0900

    4geru debug

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class WishesController < ApplicationController
-  before_action :set_wish, only: [:show, :edit, :update, :destroy]
   def index
     @wishes = Wish.all
   end
@@ -23,6 +22,7 @@ class WishesController < ApplicationController
   end
 
   def delete
+    @wish = Wish.find(params[:id])
     if @wish.destroy
       redirect_to wishes_path
     else
@@ -31,10 +31,6 @@ class WishesController < ApplicationController
   end
 
   private
-
-  def set_wish
-    @wish = Wish.find(params[:id])
-  end
 
   def wish_params
     params.requrie(:wish).permit(:user_name, :content)
